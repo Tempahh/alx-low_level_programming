adbee6fa8e051b7e7d3a32e970a766d1a7034b7f
Prosper <prosperorjiogo1999@gmail.com>
2023-07-14 04:33:03 +0100

    C programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main- entry point
+*exit: 1 (success)
+*/
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+system("rm barhammounafissa > sorti 2>&1");
+exit (1);
+}
