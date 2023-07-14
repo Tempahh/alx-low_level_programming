06852c1d2f35ce52c3f66074052adeedb448677f
Prosper <prosperorjiogo1999@gmail.com>
2023-07-14 23:20:49 +0100

    c-programming day2

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- main block
+ * Description: prints numbers 0-9
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+c = 0;
+while (c < 10)
+{
+printf("%d", c);
+c++;
+}
+return (0);
+}
