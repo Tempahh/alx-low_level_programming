83bd0c4483dea3654cfd86837b5e0fe421230918
Prosper <prosperorjiogo1999@gmail.com>
2023-07-14 23:51:39 +0100

    c-programming day2

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- main block
+ * Description: prints numbers 0-9
+ * Return: 0
+ */
+int main(void)
+{
+char d;
+int c;
+for (c = 0; c < 10; c++)
+{
+putchar(c + '0');
+}
+for (d = 'a'; d < 'g'; d++)
+{
+putchar(d
+);
+}
+putchar('\n');
+return (0);
+}
