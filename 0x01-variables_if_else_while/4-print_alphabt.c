bf92c1ef7a418e15fc35aa40ef4f77a9f166556c
Prosper <prosperorjiogo1999@gmail.com>
2023-07-14 23:15:11 +0100

    c-programming day2

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,15 @@
  * Description: prints alphabet in lowercase and excludes E and Q.
  * Return: 0
  */
-int main(void) 
+int main(void)
 {
 char c = 'a';
-while (c <= 'z') 
+while (c <= 'z')
 {
 if (c != 'e' && c != 'q')
 putchar(c);
 c++;
 }
 putchar('\n');
-return 0;
+return (0);
 }
