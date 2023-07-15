5a531f894280c1020728c4cb00273cabaa9c805b
Prosper <prosperorjiogo1999@gmail.com>
2023-07-15 03:14:57 +0100

    c-programming day2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,31 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
+/**
+ * main - main block
+ * Description: Print all possible different combinations of 3 digits.
+ * Numbers must be separated by commas and a space.
+ * The 3 digits must be different.
+ * Return: 0
+ */
 int main(void)
 {
-    int a, b, c, d;
-    for (a = 0; a < 1000; a++)
-    {
-        b = a % 10; /* fetches the single digits */
-        c = a / 10; /* fetches the double digits */
-        d = (a / 10) % 10;
-
-        if (c < d && d < b)
-        {
-            putchar(c + '0');
-            putchar(b + '0');
-            putchar(d + '0');
-
-            if (a < 700)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+int a, b, c, d;
+for (a = 0; a < 1000; a++)
+{
+b = a / 100; /* hundreds */
+c = (a / 10) % 10; /* tens */
+d = a % 10; /* singles */
+if (b < c && c < d)
+{
+putchar(b + '0');
+putchar(c + '0');
+putchar(d + '0');
+if (a < 700)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Print all possible different combinations of 3 digits.
- * Numbers must be separated by commas and a space.
- * The 3 digits must be different.
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
  * Return: 0
  */
 int main(void)
 {
-	int a, b, c, d;
+int i, j;
+int a, b, c, d;
 
-	for (a = 0; a < 1000; a++)
-	{
-b = a / 100; /* hundreds */
-c = (a / 10) % 10; /* tens */
-d = a % 10; /* singles */
-if (b < c && c < d)
+for (i = 0; i < 100; i++)
 {
+a = i / 10; /* doubles fnum */
+b = i % 10; /* singles fnum */
+
+for (j = 0; j < 100; j++)
+{
+c = j / 10; /* doubles snum */
+d = j % 10; /* singles snum */
+if (a < c || (a == c && b < d))
+{
+putchar(a + '0');
 putchar(b + '0');
+putchar(32);
 putchar(c + '0');
 putchar(d + '0');
-if (a < 700)
+if (!(a == 9 && b == 8))
 {
 putchar(44);
 putchar(32);
 }
 }
 }
-putchar('\n');
+}
+putchar(10);
 return (0);
 }
