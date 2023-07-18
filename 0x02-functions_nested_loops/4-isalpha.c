3b596ba71cd0c4737bbd05a374f233beeefc0de2
Prosper <prosperorjiogo1999@gmail.com>
2023-07-18 13:52:35 +0100

    task 3

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _isalpha - checks for alphabets
+ *@c- collecting argument
+ * Description- this program aims to find alphabets
+ * Return: Always 0.
+*/
+int _isalpha(int c)
+{
+return ((c >= 65) && (c <= 122));
+}
