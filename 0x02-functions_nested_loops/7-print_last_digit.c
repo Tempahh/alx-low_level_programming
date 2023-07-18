aecbe67e9be1f1246d45d215502887fa8f249032
Prosper <prosperorjiogo1999@gmail.com>
2023-07-18 18:14:15 +0100

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @a: parameter used to collect number
+ * Return - always o.
+*/
+int print_last_digit(int)
+{
+int a;
+_putchar ("%d" + a % 10);
+}
