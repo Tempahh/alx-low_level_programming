995267444cd2aba757dfcfb837fab23c2eebdccf
Prosper <prosperorjiogo1999@gmail.com>
2023-07-18 20:19:03 +0100

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,9 +12,12 @@ for(a = 0; a < 24; a++)
 {
 for (b = 0; b < 60; b++)
 {
-_putchar(a + 48);
+_putchar(a / 10 + 48);
+_putchar(a % 10 + 48);
 _putchar(':');
-_putchar(b + 48);
+_putchar(b / 10 + 48);
+_putchar(b % 10 + 48);
+_putchar('\n');
 }
 }
 }
