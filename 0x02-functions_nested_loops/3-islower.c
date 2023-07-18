94df370be15a2f73f1308559d13dd6214334d6d2
Prosper <prosperorjiogo1999@gmail.com>
2023-07-18 13:24:26 +0100

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * islower - checks for lower case letters
+ * Description- this program aims to find lower case haracters
+*/
+void islower(void)
+{
+char a;
+if(a = 'a' && a <= 'z')
+{
+return (1);
+}
+return (0);
+}
