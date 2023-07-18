21fe9736f882a59826e730cd73d970031a4e2d9c
Prosper <prosperorjiogo1999@gmail.com>
2023-07-18 20:12:05 +0100

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,10 +7,10 @@
  */
 void jack_bauer(void)
 {
-int a = 0, b = 0;
-for(a < 24; a++;)
+int a, b;
+for(a = 0; a < 24; a++)
 {
-for (b < 60; b++;)
+for (b = 0; b < 60; b++)
 {
 _putchar(a + 48);
 _putchar(':');
