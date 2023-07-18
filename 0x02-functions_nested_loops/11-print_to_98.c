01452b8136097cc5a25d819add300e41d4acbe47
Prosper <prosperorjiogo1999@gmail.com>
2023-07-19 00:26:16 +0100

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,8 +7,19 @@
  */
 void print_to_98(int n)
 {
-for (n; n <= 98; n++)
-_putchar(n + 48);
-_putchar(',');
-_putchar(' ');
+if (n > 98)
+{
+while (n >= 98)
+{
+printf("%d, ", n);
+n--;
+}
+}
+else
+{
+while (n <= 98)
+{
+    printf("%d, ", n);
+}
+}
 }
