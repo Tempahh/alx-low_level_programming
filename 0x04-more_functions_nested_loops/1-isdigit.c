ccba4efa97bbf75dc73db463e6a572a1a67e43f2
Prosper <prosperorjiogo1999@gmail.com>
2023-07-20 13:41:39 +0100

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * isdigit - entry point
+ * _isdigit - entry point
  * Description - checks for characters between 0 and 9
  * @c: collects arguments for the function
  * Return - 1 & 0 
 */
-int isdigit(int c)
+int _isdigit(int c)
 {
 if ((c > 47) && (c < 58))
 return (1);
