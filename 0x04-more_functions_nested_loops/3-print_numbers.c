fa4093e18652e41ad0fae0e9bd25de268cd1ca45
Prosper <prosperorjiogo1999@gmail.com>
2023-07-20 14:26:08 +0100

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,12 @@
 /**
  * print_numbers - print numbers between 0 & 9
  * Description - prints numbers from 0 - 9
- * Return - Always 0.
+ * Return: Always 0.
 */
 void print_numbers(void)
 {
 char a;
-for(a = 0; a < 10; a++)
+for (a = 0; a < 10; a++)
 {
 _putchar (a + '0');
 }
