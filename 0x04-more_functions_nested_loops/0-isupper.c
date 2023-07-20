fcb1d73cd655aaa6126ffcb13148da531f78105d
Prosper <prosperorjiogo1999@gmail.com>
2023-07-20 14:20:30 +0100

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
  * _isupper - entry point
  * @c: parameter to take argument
  * Description - program to check for uppercase character
- * Return - 1 if uppercase & 0 if lowercase Success
+ * Return: 1 if uppercase & 0 if lowercase Success
 */
 int _isupper(int c)
 {
