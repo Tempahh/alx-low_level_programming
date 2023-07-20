d73f668f760ec18e0a47d1ce6695af39c9ebb0e6
Prosper <prosperorjiogo1999@gmail.com>
2023-07-20 13:47:52 +0100

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,3 +1,10 @@
+#include "main.h"
+/**
+ * mul- multiplies two variables
+ * @a: first operand
+ * @b: second operand
+ * Return- Always 0.
+*/
 int mul(int a, int b)
 {
     return ( a * b);
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,14 @@
 #include "main.h"
-
+/**
+ * print_numbers - print numbers between 0 & 9
+ * Description - prints numbers from 0 - 9
+ * Return - Always 0.
+*/
 void print_numbers(void)
 {
-    int a;
-    for (a = 0; a < 10; a++)
-    _putchar (a);
-    _putchar('\n');
-    return (0);
+int a = 0;
+while (a < 10)
+_putchar (a + '0');
+_putchar('\n');
+return (0);
 }
