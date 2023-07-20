bddae40fd11e20e4a28913313c93440faf435f79
Prosper <prosperorjiogo1999@gmail.com>
2023-07-20 14:49:08 +0100

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,22 @@
 #include "main.h"
-/***/
+/**
+ * print_diagonal - prints diagonal lines
+ * @n: parameter to collect argument
+ * Return: Always 0.
+*/
 void print_diagonal(int n)
 {
-    int a = 0;
-    while (a < n)
-    {
-        int b = 0;
-        while (b < a)
-        {
-            _putchar(' ');
-            b++;
-        }
-        _putchar('\\');
-        _putchar('\n');
-        a++;
-    }
-    
+int a = 0;
+while (a < n)
+{
+int b = 0;
+while (b < a)
+{
+_putchar(' ');
+b++;
+}
+_putchar('\\');
+_putchar('\n');
+a++;
+}
 }
