47ae34536b6e5d2a1b0f1e4ff42e13166f2a111a
Prosper <prosperorjiogo1999@gmail.com>
2023-07-20 21:24:09 +0100

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,6 +2,7 @@
 /**
  * print_lines - prints lines
  * @n: parameter for collecting arguments
+ * Description:  draws a straight line in the terminal
  * Return: Always 0.
 */
 void print_line(int n)
