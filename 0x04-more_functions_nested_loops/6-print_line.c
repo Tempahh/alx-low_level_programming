37299f00dd719c7d99766f0d52c6e9a167d2a75f
Prosper <prosperorjiogo1999@gmail.com>
2023-07-20 14:43:32 +0100

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,16 @@
 #include "main.h"
-/***/
+/**
+ * print_lines - prints lines
+ * @n: parameter for collecting arguments
+ * Return: Always 0.
+*/
 void print_line(int n)
 {
-    int a =0;
-    while (a < n)
-    {
-        _putchar ('_');
-        a++;
-    }
-    _putchar ('\n');
+int a =0;
+while (a < n)
+{
+_putchar ('_');
+a++;
+}
+_putchar ('\n');
 }
