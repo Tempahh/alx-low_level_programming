f9868689bdbb5154d7e51aaad124e8eac6bd2247
Prosper <prosperorjiogo1999@gmail.com>
2023-07-20 14:31:22 +0100

    task 4

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,15 @@
 #include "main.h"
-/***/
+/**
+ * more_numbers - prints numbers between 0-14
+ * Reurn: Always 0.
+*/
 void more_numbers(void)
 {
-    int a;
-    for (a = 0; a < 15; a++)
-    {
-        _putchar(a);
-    }
-    _putchar('\n');
-    return (0);
+int a;
+for (a = 0; a < 15; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+return (0);
 }
