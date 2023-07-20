74f8ab17d56ba40be749f1c0326dd2db10dba7f3
Prosper <prosperorjiogo1999@gmail.com>
2023-07-20 13:57:23 +0100

    task 4

--- 0x04-more_functions_nested_loops/4-print_more_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_more_numbers.c
@@ -6,12 +6,17 @@
 */
 void print_most_numbers(void)
 {
-char a;
+int a;
 for (a = 0; a < 10; a++)
 {
 if (a == 2 || a == 4)
-break;
+{
+continue;
+}
+else
+{
 _putchar (a + '0');
+}
 }
 _putchar('\n');
 }
