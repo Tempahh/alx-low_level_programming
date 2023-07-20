128906b5c837914c50c96e16a94da3d10eedc537
Prosper <prosperorjiogo1999@gmail.com>
2023-07-20 20:25:12 +0100

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints 1 - 100 with factorial func
+ *Return: Always 0.
+*/
+int main(void)
+{
+int a;
+for (a = 1; a <= 100; a++)
+{
+if (a % 3 == 0 && a % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+if (a % 5 == 0)
+{
+printf("Buzz ");
+}
+}
+printf("%d ", a);
+}
+return (0);
+}
