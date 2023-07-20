9d7ba7c2c488addeae06e83172856942db0504e5
Prosper <prosperorjiogo1999@gmail.com>
2023-07-20 20:30:19 +0100

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,26 @@
 #include "main.h"
-/***/
+/**
+ * print_square- prints #
+ * @size: parameter to collect args
+ * Return: Always 0.
+*/
 void print_square(int size)
 {
-    int a = 0;
-    while (a < size)
-    {
-        int b = 0;
-        while (b > a)
-        {
-            if (size == 0 || size < 0)
-            {
-                _putchar('\n');
-                break;
-            }
-            _putchar('#');
-            b++;
-        }
-        _putchar('\n');
-        a++;
-    }    
+int a = 0;
+while (a < size)
+{
+int b = 0;
+while (b > a)
+{
+if (size == 0 || size < 0)
+{
+_putchar('\n');
+break;
+}
+_putchar('#');
+b++;
+}
+_putchar('\n');
+a++;
+}    
 }
