f1e00d2a1586f6964ce42ee39446c04a1c8f2c2d
Prosper <prosperorjiogo1999@gmail.com>
2023-07-24 19:25:44 +0100

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string
+ * @str: parameter to collects argument
+ * Return: Always 0.
+*/
+void _puts(char *str)
+{
+printf("%s ", str);
+}
