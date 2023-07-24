c193c995cebe4d7617717da33b76d9ac6bdcbcca
Prosper <prosperorjiogo1999@gmail.com>
2023-07-24 21:11:11 +0100

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts - prints a string
+ * puts2 - prints a string
  * @str: parameter to collects argument
  * Return: Always 0.
 */
 void puts2(char *str)
 {
+int b = 0;
 while (*str != '\0')
+{
+if (b % 2 == 0)
 {
 _putchar(*str);
 str = str + 2;
+b++;
+}
 }
 _putchar('\n');
 }
