fe92b0ab5ca6ea0a245d42e992b22c177495d237
Prosper <prosperorjiogo1999@gmail.com>
2023-07-24 18:39:23 +0100

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - calculates string length
+ * @s: a parameter to collect argument
+ * Return: Alway 0.
+*/
+int _strlen(char *s)
+{
+int a, b = 0;
+for (a = 0; a < *s; a++)
+{
+b++;
+}
+}
