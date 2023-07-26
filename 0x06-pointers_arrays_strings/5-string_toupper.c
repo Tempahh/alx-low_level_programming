661c66f93f48456cc22c6753810551e8656db4b6
Prosper <prosperorjiogo1999@gmail.com>
2023-07-26 22:13:37 +0100

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,12 @@
  */
 char *string_toupper(char *s)
 {
-for (int i = 0; s[i] != '\0'; i++)
-{
-if (s[i] >= 'a' && s[i] <= 'z')
-{
-s[i] -= 32;
-}
-}
-return (s);
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+	}
+	return (s);
 }
