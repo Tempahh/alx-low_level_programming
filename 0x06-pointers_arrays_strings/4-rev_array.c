df05c1b00fb73a3848277854377f6d4a480daa04
Prosper <prosperorjiogo1999@gmail.com>
2023-07-26 21:47:25 +0100

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,10 @@
 #include "main.h"
-/***/
+/**
+ * reverse_array - reverse
+ * @a : pointer to int params
+ * @n : pointer to int params
+ * Return: no
+ */
 void reverse_array(int *a, int n)
 {
 int i = 0;
