bd5735c85754a4a116bb2274f3842e1d9744ec03
Prosper <prosperorjiogo1999@gmail.com>
2023-07-31 14:05:59 +0100

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_memcpy - function to copy memory area
+ * @dest: dest for the memory copy
+ * @src: memory source to copy
+ * @n: number of times to copy the memory
+ * Return: Alwyas 0.
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int *p = dest;
+while (n > 0)
+{
+*dest = *src;
+dest++;
+src++;
+n--;
+}
+return (p);   
+}
