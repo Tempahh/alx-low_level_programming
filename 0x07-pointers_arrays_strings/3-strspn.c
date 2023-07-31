9a4b325350bd88a4e3efccc828e6f00a0f72f33e
Prosper <prosperorjiogo1999@gmail.com>
2023-07-31 20:21:19 +0100

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strspn - search a string for a set of bytes
+ * @s: char string array
+ * @accept: char array to check bytes with
+ * Return: Number of bytes in the intial segment of `s`
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int a = 0, b = 0, c = 0;
+while (s[a] != '\0')
+{
+while (accept[b] != '\0')
+{
+if (s[a] == accept[b])
+{
+c++;
+break;
+}
+b++;
+}
+a++;
+}
+return(c);
+}
