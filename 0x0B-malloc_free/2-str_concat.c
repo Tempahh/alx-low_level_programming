d9ecc88ad6cf8da798b0bf389ffbea64f62457c2
Prosper <prosperorjiogo1999@gmail.com>
2023-08-08 12:51:47 +0100

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,8 +17,8 @@ if (new_pointer == NULL)
 return NULL;
 for (i = 0; s1[i]; i++)
 new_pointer[i] = s1[i];
-for (j; s2[j]; j++)
-new_pointer[i] = s2[j];
+for (j = 0; s2[j]; j++)
+new_pointer[i + j] = s2[j];
 new_pointer[c] = '\0';
 return (new_pointer);
 }
