dc1d36410f5ada5aa0543be223170e7cedd20711
Prosper <prosperorjiogo1999@gmail.com>
2023-08-08 11:56:31 +0100

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - function to copy string to a new pointer
+ * @str: argument parameter
+ * Return: the new string array
+*/
+char *_strdup(char *str)
+{
+int i = 0, j = 0;
+char *new_array;
+while (str != '\0')
+i++;
+new_array = malloc((i + 1) * sizeof(char));
+if (new_array != NULL)
+{
+while (str[j])
+{
+new_array[i] = str[j];
+j++;
+}
+new_array[i] = '\0';
+}
+return (new_array);
+}
