c07882363c155cbcecbe936644cc8b8bbc184541
Prosper <prosperorjiogo1999@gmail.com>
2023-08-08 13:12:23 +0100

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,9 @@ char *str_concat(char *s1, char *s2)
 {
 char *new_pointer;
 int i = 0, j = 0, c = 0;
-if ( s1 == NULL)
+if (s1 == NULL)
 s1 = "";
-if(s2 == NULL)
+if (s2 == NULL)
 s2 = "";
 for (i = 0; s1[i] || s2[i]; i++)
 c++;
