3b52e4ee865fdbcf0b567932a2a4a8a3e834f93a
Prosper <prosperorjiogo1999@gmail.com>
2023-08-10 12:31:17 +0100

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,20 +9,21 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *new_pointer;
-    int i = 0, j = 0, c = 0;
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-    for (i = 0; s1[i] || s2[i]; i++)
-        c++;
-    new_pointer = malloc(sizeof(char) * c);
-    if (new_pointer == NULL)
-        return (NULL);
-    for (i = 0; s1[i]; i++)
-        new_pointer[j++] = s1[i];
-    for (i = 0; i < n && s2[i]; i++)
-        new_pointer[j++] = s2[i];
-    return (new_pointer);
+char *new_pointer;
+int i = 0, j = 0, c = 0;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (i = 0; s1[i] || s2[i]; i++)
+c++;
+new_pointer = malloc(sizeof(char) * c);
+if (new_pointer == NULL)
+return (NULL);
+for (i = 0; s1[i]; i++)
+new_pointer[j++] = s1[i];
+for (i = 0; i < n && s2[i]; i++)
+new_pointer[j++] = s2[i];
+new_pointer[j] = '\0';
+return (new_pointer);
 }
