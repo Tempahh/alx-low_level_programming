5b027725dcc893fec80ebf79b85ce4b7250e500d
Prosper <prosperorjiogo1999@gmail.com>
2023-08-10 13:11:21 +0100

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
  * _calloc - function to allocate memory for a new array
  * @nmemb: function parameter
  * @size: function parameter
- * Return: returns new pointer
+ * Return: returns new pointer on success else null value
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -11,18 +11,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *char_ptr;
 	if (nmemb == 0 || size == 0)
-		return NULL;
+		return (NULL);
 
-	unsigned int total_size = nmemb * size;
 
-	void *ptr = malloc(total_size);
+	void *ptr = malloc(nmemb * size);
 
 	if (!ptr)
 		return (NULL);
 
 	char_ptr = ptr;
     
-	for (i = 0; i < total_size; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		char_ptr[i] = '\0';
 
 	return (ptr);
