7268371d4db74d35204cbd520663001c4736e6ea
Prosper <prosperorjiogo1999@gmail.com>
2023-08-10 10:50:13 +0100

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - function to allocate memory
+ * @b: function parameter
+ * Return: new pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+    unsigned int *pointer = malloc(b);
+    if(pointer == NULL)
+        return (98);
+    return (pointer);
+}
