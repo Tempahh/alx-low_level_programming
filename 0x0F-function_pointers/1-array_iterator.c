a0a6c3ea2b5710d1818d559c99c60650dcdefa97
Prosper <prosperorjiogo1999@gmail.com>
2023-08-16 21:15:02 +0100

    Tasks 0 & 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_iterator - function to call a function pointer 
+ * @array: function parameter
+ * @size: function parameter
+ * @action: function pointer
+*/
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+	if(action && array)
+		while (size--)
+			action(*(array++));
+}
