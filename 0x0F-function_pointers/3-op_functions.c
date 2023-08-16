373a0a9cdc65cf80456d01a93aca3a7001cde219
Prosper <prosperorjiogo1999@gmail.com>
2023-08-16 23:42:33 +0100

    Tasks 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,21 +1,22 @@
-#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * op_add - adds two integers
- * @a: first integer
- * @b: second integer
- * Return: sum of two integers
+ * op_add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 numbers
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
-
 /**
- * op_sub - subtracts two integers
- * @a: first integer
- * @b: second integer
- * Return: difference of two integers
+ * op_sub - subtraction operator
+ * @a: first in
+ * @b: second int
+ * Return: difference of 2 numbers
  */
 int op_sub(int a, int b)
 {
@@ -23,10 +24,10 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - multiply two integers
- * @a: first integer
- * @b: second integer
- * Return: product of two integers
+ * op_mul - product of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: product of the numbers
  */
 int op_mul(int a, int b)
 {
@@ -34,14 +35,14 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - divides two integers
- * @a: first integer
- * @b: second integer
- * Return: quotient of two integers
+ * op_div - division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: result of the division of the numbers
  */
 int op_div(int a, int b)
 {
-	if (!b)
+	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
@@ -50,14 +51,14 @@ int op_div(int a, int b)
 }
 
 /**
- * op_mod - calculates modulus two integers
- * @a: first integer
- * @b: second integer
- * Return: modulus of two integers
+ * op_mod - get remainder of the division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: remainder of division of the numbers
  */
 int op_mod(int a, int b)
 {
-	if (!b)
+	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
