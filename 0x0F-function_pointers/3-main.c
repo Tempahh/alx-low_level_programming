a3f867b678ad20d6e737f18d4db83df96def8753
Prosper <prosperorjiogo1999@gmail.com>
2023-08-16 23:36:19 +0100

    Tasks 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,22 +1,41 @@
 #include "3-calc.h"
-/**
- * main - performs simple operations
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 if no errors
- */
-int main (int argc, char *argv[])
-{
-int i, j;
-int (*func) (int, int);
+#include <stdlib.h>
+#include <stdio.h>
 
-if (argc != 4)
-printf("Error"), exit (98);
-func = get_op_func(argv[2]);
-if (!func)
-printf("Error \n"), exit (99);
-i = atoi(argv[1]);
-j = atoi(argv[3]);
-printf("%i \n", func(i, j));
-return (0);
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: string of arguments in array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*o)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = get_op_func(argv[2]);
+	if (o == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", o(a, b));
+
+	return (0);
 }
