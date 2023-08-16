dc6242bebc06a921ef19ace8f49a661ec5133cbf
Prosper <prosperorjiogo1999@gmail.com>
2023-08-16 23:05:49 +0100

    Tasks 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,22 @@
+#include "3-calc.h"
+/**
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if no errors
+ */
+int main (int argc, char *argv[])
+{
+int i, j;
+int (*func) (int, int);
+
+if (argc != 4)
+printf("Error"), exit (98);
+func = get_op_func(argv[2]);
+if (!func)
+printf("Error \n"), exit (98);
+i = atoi(argv[1]);
+j = atoi(argv[3]);
+printf("%d \n", func(i, j));
+return (0);
+}
