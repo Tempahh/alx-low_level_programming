942527ed8bb62fc5335f389d3f06e5347b5ec186
Prosper <prosperorjiogo1999@gmail.com>
2023-08-16 23:29:30 +0100

    Tasks 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "function_pointers.h"
 
 /**
- * main - check the code for Holberton School students.
- * @argc: argument count.
- * @argv: argument vector.
- *
- * Return: Always 0.
+ * main - print opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: vector of arguments
+ * Return: 0 if no errors
  */
 int main(int argc, char *argv[])
 {
-	char *opc = (char *) main;
-	int i, nbytes;
+	register int i, num;
+	char *ptr = (char *)main;
 
 	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-
-	nbytes = atoi(argv[1]);
-
-	if (nbytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	for (i = 0; i < nbytes; i++)
-	{
-		printf("%02x", opc[i] & 0xFF);
-		if (i != nbytes - 1)
-			printf(" ");
-	}
-
-	printf("\n");
+		printf("Error\n"), exit(1);
+	num = atoi(argv[1]);
+	if (num < 0)
+		printf("Error\n"), exit(2);
+	for (i = 0; i < num - 1; i++)
+		printf("%02hhx ", ptr[i]);
+	printf("%02hhx\n", ptr[i]);
 	return (0);
 }
