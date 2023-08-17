e1fe74e1986dce517878624d747a834a6a30d6b8
Prosper <prosperorjiogo1999@gmail.com>
2023-08-17 21:49:11 +0100

    tasks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,11 +11,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list arg_list;
 	unsigned int a;
+	char *str;
 
 	va_start(arg_list, n);
 	for (a = 0; a < n; a++)
 	{
-		char *str = va_arg(arg_list, char *);
+		str = va_arg(arg_list, char *);
 
 		if (str == NULL)
 			printf("nil");
