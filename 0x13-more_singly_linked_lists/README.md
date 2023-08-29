a54d1e867d12ad5ee6f3d6f8e870df04d4de4ad1
Prosper <prosperorjiogo1999@gmail.com>
2023-08-29 17:15:03 +0100

    more singly link lists tasks

---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+projects for more singly linked lists
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+ #endif
