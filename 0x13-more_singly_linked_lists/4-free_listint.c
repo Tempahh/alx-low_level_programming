ac6c306787e0e46471ff22a18fc9871e3ee8b2ec
Prosper <prosperorjiogo1999@gmail.com>
2023-08-29 21:36:46 +0100

    more singly linked list tasks

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,9 +7,9 @@
  */
 void free_listint(listint_t *head)
 {
+    listint_t *temp = head;
     while (head != NULL)
     {
-        listint_t *temp = head;
         head = head->next;
         free(temp);
     }
