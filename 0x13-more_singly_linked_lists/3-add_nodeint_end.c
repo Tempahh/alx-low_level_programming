d2921e0d3b49066b43186f1e8ebbc704020d1ecb
Prosper <prosperorjiogo1999@gmail.com>
2023-08-29 21:32:08 +0100

    more singly linked list tasks

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,25 +10,25 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *newnode = (listint_t *)malloc(sizeof(listint_t));
-    listint_t *pointer = *head;
+	listint_t *newnode = (listint_t *)malloc(sizeof(listint_t));
+	listint_t *pointer = *head;
 
-    if(newnode == NULL)
-    {
-        return (NULL);
-    }
-    newnode->n = n;
-    newnode->next = NULL;
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->n = n;
+	newnode->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = newnode;
-        return(newnode);
-    }
-    while (pointer->next != NULL)
-    {
-        pointer = pointer->next;
-    }
-    pointer->next = newnode;
-    return (newnode);
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+	while (pointer->next != NULL)
+	{
+		pointer = pointer->next;
+	}
+	pointer->next = newnode;
+	return (newnode);
 }
