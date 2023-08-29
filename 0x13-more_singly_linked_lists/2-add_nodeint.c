59b91641a38e732036346b43a3156ee9b2c772b7
Prosper <prosperorjiogo1999@gmail.com>
2023-08-29 21:19:51 +0100

    more simgly linked list tasks

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_nodeint - a function ...
+ * @head: the list
+ * @n: the chaine
+ *
+ * Return: 1 or 0
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t *newnode = (listint_t *)malloc(sizeof(listint_t));
+
+    if(!newnode)
+    {
+        return(NULL);
+    }
+    newnode->n = n;
+    newnode->next = *head;
+    *head = newnode;
+
+    return (*head);
+}
